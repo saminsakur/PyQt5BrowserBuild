65eb635314878e27060766016148aae86fe29054
saminsakur8@gmail.com <saminsakur8@gmail.com>
2021-05-30 23:30:02 +0600

    created Basic browser

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("Hello world!")
-print("hello chibi")
+import sys
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebChannel import *
+from PyQt5.QtWebEngine import *
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+
+
+class mainWindow(QMainWindow):
+    def __init__(self):
+        super(mainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("https://www.google.com"))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName("The Browser By Samin")
+window = mainWindow()
+app.exec_()
