06a2789ab37c91e612ed90aca2a8571ea2b3f421
samin <saminsakur06@gmail.com>
2021-10-09 12:23:59 +0600

    Initial commit 🎉

--- browser/app.py
+++ browser/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import sys
+
+cli = sys.modules['flask.cli']
+cli.show_server_banner = lambda *x: None
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def server():
     return render_template("index.html")
-
 
 # app.run(debug=True)
